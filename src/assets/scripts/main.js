b44f392f283318d2efd91f5f71735cbc7018f1df
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-08-31 21:39:38 -0700

    Updated grid styling. Updated Typography styling. Changed and added more custom fonts.  Updated Homepage Tipe CMS -- updated query.

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,7 +1,12 @@
 import { HomePage } from '../tipe/home.tipe';
 
+// Header
 const nav = document.getElementsByTagName('nav')[0];
 const logo = document.querySelector('.logo');
+
+// Intro Section
+const introHeading = document.querySelector('#intro__heading');
+const introDesc = document.querySelector('#intro__desc');
 
 HomePage.pageTitle = document.getElementsByTagName('title')[0];
 
@@ -11,9 +16,12 @@ for (let navItem in HomePage.navItems) {
     nav.innerHTML += `<li class="nav__item"><a href="${ HomePage.navItems[navItem] }">${navItem}</a></li>`;
   } else {
     if (HomePage.logoGroup.logoImage === null) {
-      logo.innerHTML = `<a href="${HomePage.navItems[navItem]}">${HomePage.logoGroup.logoText}</a>`;
+      logo.innerHTML = `<a id="logo__text" href="${HomePage.navItems[navItem]}">${HomePage.logoGroup.logoText}</a>`;
     } else {
       logo.innerHTML = `<a href="${HomePage.navItems[navItem]}"><img src="${HomePage.logoGroup.logoImage.url}"></a>`;
     }
   }
 }
+
+introHeading.innerHTML = HomePage.sectionBlocks.introSection.heading;
+introDesc.innerHTML = HomePage.sectionBlocks.introSection.description;
