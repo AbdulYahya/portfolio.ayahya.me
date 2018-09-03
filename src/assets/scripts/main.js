b7e675efde5122f560fb7a1322ba7a72f4082d8f
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-09-03 01:54:00 -0700

    Refactored main.js. Removed all variables and replaced w/shortcuts to document, querySelector, and querySelectorAll. Added footer copyright text to CMS and styled it a bit. Rebuilt site.

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,35 +1,36 @@
 import { HomePage } from '../tipe/home.tipe';
 
-// Header
-const nav = document.getElementsByTagName('nav')[0];
-const logo = document.querySelector('.logo');
+// Shortcut Vars
+const D = document;
+const $ = D.querySelector.bind(D);
+const $$ = (selector, startNode = D) => [
+  ...startNode.querySelectorAll(selector)
+];
 
 // Intro Section
-const introHeading = document.querySelector('#intro__heading');
-const introDesc = document.querySelector('#intro__desc');
-
-HomePage.pageTitle = document.getElementsByTagName('title')[0];
+$('title').innerHTML = HomePage.pageTitle;
 
 // prettier-ignore
 for (let navItem in HomePage.navItems) {
   if (navItem !== 'home') {
-    nav.innerHTML += `<li class="nav__item"><a href="${ HomePage.navItems[navItem] }">${navItem}</a></li>`;
+    $('nav').innerHTML += `<li class="nav__item"><a href="${ HomePage.navItems[navItem] }">${navItem}</a></li>`;
   } else {
     if (HomePage.logoGroup.logoImage === null) {
-      logo.innerHTML = `<a id="logo__text" href="${HomePage.navItems[navItem]}">${HomePage.logoGroup.logoText}</a>`;
+      $('.logo').innerHTML = `<a id="logo__text" href="${HomePage.navItems[navItem]}">${HomePage.logoGroup.logoText}</a>`;
     } else {
-      logo.innerHTML = `<a href="${HomePage.navItems[navItem]}"><img id="logo__img" src="${HomePage.logoGroup.logoImage.url}"></a>`;
+      $('.logo').innerHTML = `<a href="${HomePage.navItems[navItem]}"><img id="logo__img" src="${HomePage.logoGroup.logoImage.url}"></a>`;
     }
   }
 }
 
-introHeading.innerHTML = HomePage.sectionBlocks.introSection.heading;
-introDesc.innerHTML = HomePage.sectionBlocks.introSection.description;
+$('#intro__heading').innerHTML = HomePage.sectionBlocks.introSection.heading;
+$('#intro__desc').innerHTML = HomePage.sectionBlocks.introSection.description;
 
 let menuToggle = document.querySelector('.hamburger');
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('is-active');
-
-  if (nav.className === 'responsive') nav.className = '';
-  else nav.className += 'responsive';
+  if ($('nav').className === 'responsive') $('nav').className = '';
+  else $('nav').className += 'responsive';
 });
+
+$('#footer__cpy').innerHTML = HomePage.footerCopy.copyright;
