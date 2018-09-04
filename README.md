a971868878f78c6775ddaddb2620217d7216776c
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-09-04 03:28:05 -0700

    Created new branch for Hugo integration/migration

--- README.md
+++
@@ -1,5 +0,0 @@
-## License
-
-[![GitHub license](https://img.shields.io/github/license/ktenzel/Code_Meetup.svg?style=for-the-badge)](https://ayahya.mit-license.org)
-
-###### Copyright (c) 2018 Abdullah Yahya
--- src/index.js
+++
@@ -1,7 +0,0 @@
-// StyleSheet Imports
-import './assets/styles/fonts.css';
-import './assets/styles/grid.css';
-import './assets/styles/typography.css';
-import './assets/styles/main.css';
-// JavaScript Imports 
-import './assets/scripts/main.js';
