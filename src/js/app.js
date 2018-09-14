fe82f444f00a107e418513d8950d540795aa9b13
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-09-13 21:10:32 -0700

    Updated styling. Removed unnecessary code. Added delay to async quote fetch method.

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,12 @@
-// JS Goes here - ES6 supported
-
 const D = document;
 const $ = D.querySelector.bind(D);
-const $$ = (selector, startNode = D) => [
-  ...startNode.querySelectorAll(selector)
-];
+// const $$ = (selector, startNode = D) => [
+//   ...startNode.querySelectorAll(selector)
+// ];
 
-const menuToggle = $(".hamburger");
-const quoteWrapper = $(".quote_wrapper");
 
-menuToggle.addEventListener("click", () => {
-  menuToggle.classList.toggle("is-active");
+$(".hamburger").addEventListener("click", () => {
+  $(".hamburger").classList.toggle("is-active");
   if ($("nav").className === "responsive") {
     $("nav").className = "";
     $("main").style.padding = "4em 0 0 0";
@@ -22,19 +18,21 @@ menuToggle.addEventListener("click", () => {
 });
 
 const url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+// eslint-disable-next-line
+const timeout = (ms) => new Promise((res) => setTimeout(res, ms));
 
-const fetchQuote = (async) () =>
-  await (await fetch(url)).json();
+const fetchQuote = async() =>
+  await (await timeout(2000), await fetch(url)).json();
 
 fetchQuote()
   .then(
     (data) => {
       const quoteContent = data[0].content.replace(/\<p>|<\/p>/g, "");
-      console.log(quoteContent);
-      console.log(data[0].title);
 
-      quoteWrapper.innerHTML = `<p id="intro__desc">${quoteContent}</p>` +
-                               `<p id="intro_author">- ${data[0].title}</p>`;
+      $(".quote_wrapper").style.marginLeft = 0;
+      $(".quote_wrapper").style.width = "60vw";
+      $(".quote_wrapper").innerHTML = `<p id="intro__desc">${quoteContent}</p>` +
+                                      `<p id="intro_author">- ${data[0].title}</p>`;
     })
   .catch((reason) => console.log(reason.message));
 
