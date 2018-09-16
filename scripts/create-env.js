01db40fc5245d71b11baf850314e713586553103
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-09-16 02:03:23 -0700

    Removed dotenv-webpack. Added Netlify's CMS using an NPM pkg instead of their cdn js file. Hardcoded uploadcare public apiKey into config.yml for now.

--- scripts/create-env.js
+++
@@ -1,2 +0,0 @@
-const fs = require("fs");
-fs.writeFileSync("./.env", `API_KEY=${process.env.MEDIA_LIBRARY_PUBLIC_KEY}\n`);
---
+++ site/static/admin/admin.js
@@ -0,0 +1,4 @@
+import CMS from "netlify-cms";
+// eslint-disable-next-line
+CMS.registerPreviewTemplate("my-template", MyTemplate);
+
